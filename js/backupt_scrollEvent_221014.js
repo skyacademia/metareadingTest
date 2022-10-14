1240b9c5fec611650a6114ca39a1481ab74b24f1
skyacademia <81758664+skyacademia@users.noreply.github.com>
2022-10-14 18:19:31 +0900

    코드 리펙토링 및 페이지 전환 방식 변경
    - 코드 정리
    - 빈화면이 발생하지 않도록 페이지 전환 방식 변경

--- js/backupt_scrollEvent_221014.js
+++ js/backupt_scrollEvent_221014.js
@@ -5,6 +5,7 @@ let increasing = false;
 let content_areas = document.body.querySelectorAll(".area_block");
 let contents = document.body.querySelectorAll(".information_area");
 let content_area_height = document.querySelector(".area_block").scrollHeight;
+let topMenu_height = document.querySelector(".top_manu").offsetHeight;
 let indicaters = document.querySelectorAll(".indicateUnit");
 window.onscroll = animation;
 window.onload = loadAnimation;
@@ -25,7 +26,6 @@ function loadAnimation() {
 
     let num = Math.floor(scrollY / content_area_height);
     insertValue(num);
-    text_area = contents[num].querySelector('.information_title')
 
     document.getElementById(`${num}`).classList.add('indicateSelected');
 
@@ -37,7 +37,7 @@ function loadAnimation() {
 
         if (currentContentY_start <= scrollY && scrollY < currentContentY_end) {
             if (currentContentY_start <= scrollY && scrollY < currentContentY_start + (contents[num].offsetHeight * 0.3)) {
-                text_area.style.opacity = 0;
+                contents[num].querySelector('.information_title').style.opacity = 0;
 
                 contents[num].animate([{ opacity: 0 }, { opacity: rate }], { duration: 1000 });
                 contents[num].style.opacity = rate;
@@ -45,29 +45,29 @@ function loadAnimation() {
             else if (currentContentY_start + (contents[num].offsetHeight * 0.3) <= scrollY && scrollY < currentContentY_start + (contents[num].offsetHeight * 0.6)) {
                 rate = ((scrollY - content_areas[num].offsetTop) - (contents[num].offsetHeight * 0.3)) / Range;
                 if (rate > 0) {
-                    text_area.style.opacity = 0;
+                    contents[num].querySelector('.information_title').style.opacity = 0;
 
                     contents[num].animate([{ opacity: 0 }, { opacity: rate }], { duration: 1000 });
                     contents[num].style.opacity = rate;
 
-                    text_area.animate(
+                    contents[num].querySelector('.information_title').animate(
                         [{ opacity: 0, transform: `translate(0, 20%)` }, { opacity: rate, transform: `translate(0, ${20 - (rate * 20)}%)` }],
                         { duration: 1000 });
-                    text_area.style.opacity = rate;
-                    text_area.style.transform = `translate(0, ${20 - (rate * 20)}%)`;
+                    contents[num].querySelector('.information_title').style.opacity = rate;
+                    contents[num].querySelector('.information_title').style.transform = `translate(0, ${20 - (rate * 20)}%)`;
                 }
             }
             else if ((currentContentY_start + (contents[num].offsetHeight * 0.6) <= scrollY)) {
-                text_area.style.opacity = 0;
+                contents[num].querySelector('.information_title').style.opacity = 0;
 
                 contents[num].animate([{ opacity: 0 }, { opacity: 1 }], { duration: 1000 });
                 contents[num].style.opacity = 1;
 
-                text_area.animate(
+                contents[num].querySelector('.information_title').animate(
                     [{ opacity: 0, transform: `translate(0, 20%)` }, { opacity: 1, transform: `translate(0, 0)` }],
                     { duration: 1000 });
-                text_area.style.opacity = 1;
-                text_area.style.transform = "translate(0, 0)";
+                contents[num].querySelector('.information_title').style.opacity = 1;
+                contents[num].querySelector('.information_title').style.transform = "translate(0, 0)";
             }
         }
     }
@@ -97,31 +97,20 @@ function animation() {
 
     if (0 <= num && num <= 10) {
         let Range = contents[num].offsetHeight * 0.3;
+        let rate = (scrollY - content_areas[num].offsetTop) / Range;
         let currentContentY_start = content_area_height * num;
         let currentContentY_end = content_area_height * (num + 1);
-        let text_area = contents[num].querySelector('.information_title');
-        let rate = (scrollY - content_areas[num].offsetTop) / Range;
-        let entireRate = (scrollY - content_areas[num].offsetTop) / content_areas[num].offsetHeight;
         if (rate > 1) {
             rate = 1;
         } else if (rate < 0) {
             rate = 0;
         }
+        console.log("배경:" + rate);
         if (currentContentY_start <= scrollY && scrollY < currentContentY_end) {
             if (currentContentY_start <= scrollY && scrollY < currentContentY_start + (contents[num].offsetHeight * 0.3)) {
                 if (0 < rate && rate <= 1) {
-                    text_area.style.opacity = 0;
-                    if(num>0){
-                        if (contents[num].style.opacity < 1) {
-                            contents[num].style.opacity += rate;
-                        }
-                        if (contents[num - 1].style.opacity == 0) {
-                            contents[num - 1].style.opacity = 1;
-                        }    
-                    }
-                    else if(num==0){
-                        contents[num].style.opacity = rate;
-                    }
+                    contents[num].querySelector('.information_title').style.opacity = 0;
+                    contents[num].style.opacity = rate;
                 }
             }
             else if (currentContentY_start + (contents[num].offsetHeight * 0.3) <= scrollY && scrollY < currentContentY_start + (contents[num].offsetHeight * 0.6)) {
@@ -133,19 +122,14 @@ function animation() {
                     rate = 0;
                 }
                 if (0 < rate && rate <= 1) {
-                    text_area.style.opacity = rate;
-                    text_area.style.transform = `translate(0, ${20 - (rate * 20)}%)`;
+                    console.log("텍스트:" + rate);
+                    contents[num].querySelector('.information_title').style.opacity = rate;
+                    contents[num].querySelector('.information_title').style.transform = `translate(0, ${20 - (rate * 20)}%)`;
                 }
             } else if (currentContentY_start + (contents[num].offsetHeight * 0.6) <= scrollY) {
-                text_area.style.opacity = 1;
-                text_area.style.transform = `translate(0, 0)`;
+                contents[num].querySelector('.information_title').style.opacity = 1;
+                contents[num].querySelector('.information_title').style.transform = `translate(0, 0)`;
             }
-        }
-        if (0.7 < entireRate) {
-            changingRate = (((scrollY - content_areas[num].offsetTop) - (content_areas[num].offsetHeight * 0.7)) / content_areas[num].offsetHeight * 0.3) * 10
-            contents[num + 1].querySelector('.information_title').style.opacity = 0;
-            contents[num + 1].style.opacity = changingRate;
-            console.log(changingRate);
         }
     }
     if (scrollY == 0) {
